cff1c3e4df7c9e5223b87a75021a903595d6b15a
Waheedullah Khan <waheedullahkhan1250@gmail.com>
2022-12-30 01:10:44 +0500

    small changes

--- multiplication-tables.cpp
+++ multiplication-tables.cpp
@@ -6,56 +6,72 @@ using namespace std;
 void printMultiplicationTables(int startNum, int endNum, int tablesPerRow) {
     // Loop through start number to end number, incrementing by tablesPerRow each iteration
     for (int num = startNum; num <= endNum; num += tablesPerRow) {
-
         // Loop through 1 to 10 for each multiple
         for (int multiple = 1; multiple <= 10; multiple++) {
-            
             // Loop through the tablesPerRow and print each table
             for (int tableNum = 0; tableNum < tablesPerRow; tableNum++) {
                 // Break if num + tableNum is greater than endNum
-                if (num + tableNum > endNum) break;
+                if (num + tableNum > endNum) {
+                    break;
+                }
                 cout << num + tableNum << " x " << multiple << " = " << (num + tableNum) * multiple << "\t";
             }
-
             cout << endl;
         }
-        
         cout << endl;
     }
 }
 
 int main() {
     // Declare variables to hold start, end, and tablesPerRow
-    int startNum, endNum, tablesPerRow;
+    int startNum = 0, endNum = -1;
+    int tablesPerRow = 0;
     char choice;
 
-    cout << "This program will print a multiplication table from a given start number to a given end number." << endl << endl;
+    cout << " ~> This program will print a multiplication table from a given start number to a given end number." << endl << endl;
 
     // Loop until user chooses to exit
     while (true) {
+        
         cout << "Enter the number to start the table at: ";
         cin >> startNum;
 
         cout << "Enter the number to end the table at: ";
         cin >> endNum;
 
-        cout << "* Reduce the number of tables per row if the output is not displayed correctly." << endl;
-        cout << "Enter maximum number of multiplication tables per row: ";
+        // Input validation for startNum and endNum
+        while (startNum > endNum) {
+            cout << "Error: The end number must be greater than the start number." << endl;
+
+            cout << "Enter the number to start the table at: ";
+            cin >> startNum;
+
+            cout << "Enter the number to end the table at: ";
+            cin >> endNum;
+        }
+
+        cout << "* Reduce maximum number of tables per row if the output is not displayed correctly." << endl;
+        cout << "Enter maximum number of tables per row: ";
         cin >> tablesPerRow;
 
-        // Check if start number is less than or equal to end number
-        if (startNum <= endNum) {
-            cout << endl;
+        // Input validation for tablesPerRow
+        while (tablesPerRow < 1) {
+            cout << "Error: The maximum number of tables per row must be greater than 0." << endl;
 
-            // Call function to generate and print multiplication tables
-            printMultiplicationTables(startNum, endNum, tablesPerRow);
-
-            cout << endl;
-        }
-        else {
-            cout << "Error: Start number must be less than or equal to end number." << endl;
+            cout << "Enter maximum number of tables per row: ";
+            cin >> tablesPerRow;
         }
 
+        // For formatting
+        cout << endl;
+
+        // Call function to generate and print multiplication tables
+        printMultiplicationTables(startNum, endNum, tablesPerRow);
+
+        // For formatting
+        cout << endl;
+
+        // Prompt user to either exit or continue
         cout << "Enter 'e' or 'E' to exit, or any other character to continue: ";
         cin >> choice;
 
